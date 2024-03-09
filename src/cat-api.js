d8db8fc9f92447ae61d3ff762b948d450db178cb
Yevgen9 <leon020584@gmail.com>
2024-03-09 11:56:04 +0200

    commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,22 +2,48 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_2Ho3xjSdPQtSkxWtmErDvQyc0WUPB0kqOnDeHwNM5odZyrTLx0mIyezJlu9nIhnC';
 
-export function fetchBreeds() {
-  return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+// export function fetchBreeds() {
+//   return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+// export function fetchCatByBreed(breedId) {
+//   return fetch(
+//     `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+//   ).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   });
+// }
+
+export async function fetchBreeds() {
+  try {
+    const response = await fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`);
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
-  });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
-export function fetchCatByBreed(breedId) {
-  return fetch(
-    `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-  ).then(response => {
+export async function fetchCatByBreed(breedId) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+    );
     if (!response.ok) {
       throw new Error(response.status);
     }
     return response.json();
-  });
+  } catch (error) {
+    throw new Error(error);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -18,8 +18,61 @@ error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
 
 let arrBreedsId = [];
-fetchBreeds()
-  .then(data => {
+
+// fetchBreeds()
+//   .then(data => {
+//     data.forEach(element => {
+//       arrBreedsId.push({ text: element.name, value: element.id });
+//     });
+//     new SlimSelect({
+//       select: breedSelect,
+//       data: arrBreedsId,
+//     });
+//   })
+//   .catch(onFetchError);
+
+// breedSelect.addEventListener('change', onSelectBreed);
+
+// function onSelectBreed(event) {
+//   loaderRef.classList.replace('is-hidden', 'loader');
+
+//   breedSelect.classList.add('is-hidden');
+//   divCatInfo.classList.add('is-hidden');
+
+//   const breedId = event.currentTarget.value;
+//   fetchCatByBreed(breedId)
+//     .then(data => {
+//       loaderRef.classList.replace('loader', 'is-hidden');
+
+//       breedSelect.classList.remove('is-hidden');
+//       const { url, breeds } = data[0];
+
+//       divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+//       divCatInfo.classList.remove('is-hidden');
+//     })
+//     .catch(onFetchError);
+// }
+
+// function onFetchError(error) {
+//   breedSelect.classList.remove('is-hidden');
+//   loaderRef.classList.replace('loader', 'is-hidden');
+
+//   Notify.failure(
+//     'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+//     {
+//       position: 'center-center',
+//       timeout: 5000,
+//       width: '400px',
+//       fontSize: '24px',
+//     }
+//   );
+// }
+
+
+
+async function initializeBreeds() {
+  try {
+    const data = await fetchBreeds();
     data.forEach(element => {
       arrBreedsId.push({ text: element.name, value: element.id });
     });
@@ -27,29 +80,33 @@ fetchBreeds()
       select: breedSelect,
       data: arrBreedsId,
     });
-  })
-  .catch(onFetchError);
+  } catch (error) {
+    onFetchError(error);
+  }
+}
+
+initializeBreeds();
 
 breedSelect.addEventListener('change', onSelectBreed);
 
-function onSelectBreed(event) {
+async function onSelectBreed(event) {
   loaderRef.classList.replace('is-hidden', 'loader');
-
   breedSelect.classList.add('is-hidden');
   divCatInfo.classList.add('is-hidden');
 
   const breedId = event.currentTarget.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loaderRef.classList.replace('loader', 'is-hidden');
 
-      breedSelect.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
+  try {
+    const data = await fetchCatByBreed(breedId);
+    loaderRef.classList.replace('loader', 'is-hidden');
+    breedSelect.classList.remove('is-hidden');
+    const { url, breeds } = data[0];
 
-      divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-      divCatInfo.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
+    divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+    divCatInfo.classList.remove('is-hidden');
+  } catch (error) {
+    onFetchError(error);
+  }
 }
 
 function onFetchError(error) {
